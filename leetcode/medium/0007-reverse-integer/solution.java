e6da9c77536c96c735e76b46d730045c7268c833
Abhisar kumar <abhisarkumar70@gmail.com>
2026-08-24 22:29:50 +0530

    ✅ Reverse Integer | Medium | Java | Runtime: 1 ms (99.98%) | Memory: 42.6 MB | 2026-08-24

--- leetcode/medium/0007-reverse-integer/solution.java
+++ leetcode/medium/0007-reverse-integer/solution.java
@@ -1,14 +1,16 @@
 class Solution {
     public int reverse(int x) {
 
-        int rev = 0;
+        long rev = 0;
 
         while (x != 0) {
 
            rev = rev*10+x%10;
            x/=10;
         }
-if(rev<Long.MIN_VALUE || rev>Long.MAX_VALUE) return 0;
+    if(rev>Integer.MAX_VALUE || rev<Integer.MIN_VALUE) {
+          return 0;
+       }
         return (int) rev;
     }
 }
